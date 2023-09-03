b53edaeb0e1f6566f5fba153bb917f4aff988fef
scarlettdong <sdong5@uncc.edu>
2023-09-03 15:05:27 -0400

    ITCS5166 assignments

---
+++ week02/uncc.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UNCC - Scarlett Dong</title>
+</head>
+<body>
+    <h1>Scarlett - testing page</h1>
+    <hr>
+        <?php echo 9 * 13 ?>
+    
+
+</body>
+</html>
